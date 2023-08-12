9c2f4401dabb4e53d264373a6cbeae2183ce7f27
Sebastien Klay <sebklay@hotmail.com>
2023-08-12 15:12:34 +0200

    increase axios timeout limit for testing purpose

--- src/components/UserInfos.vue
+++ src/components/UserInfos.vue
@@ -106,6 +106,8 @@
 
     const backend_url = 'https://foobi.jcloud.ik-server.com'
 
+    axios.defaults.timeout = 10000;
+
     const day_of_week = ref(['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'])
 
     const route = useRoute()
